404c35b6c23e8e4e77ffa4592debb24b958f739d
董家宏 <dongjiahong@hotmail.com>
2017-06-21 13:53:45 +0800

    add random button

--- getImgUrls.lua
+++ getImgUrls.lua
@@ -1,20 +1,36 @@
 require "os"                                                                                           
 local json = require "cjson"                                                                           
 
-ngx.header['Access-Control-Allow-Origin'] = "*"                                                        
+--ngx.header['Access-Control-Allow-Origin'] = "*"                                                        
 ngx.header['Content-Type'] = "application/json"                                                        
+math.randomseed(tostring(os.time()):reverse():sub(1, 7))
+
+local args = ngx.req.get_uri_args()
+local rand = args.rand or "0"
 
 os.execute(" ls -lh /pdata1/tmp_picture | sort -nk5 | awk '{if(NF>=9){print $9}}' > res")                                                                                                                             
 
 local res={ }                                                                                          
 local file = io.open("res")                                                                            
 local idx=0                                                                                            
-for line in file:lines() do                                                                            
-    if idx == 100 then                                                                                   
-        break                                                                                          
-    end                                                                                                
-    table.insert(res, line)                                                                            
-    idx = idx + 1                                                                                      
-end                                                                                                    
+if rand == "0" then
+    for line in file:lines() do                                                                            
+        if idx == 100 then                                                                                   
+            break                                                                                          
+        end                                                                                                
+        table.insert(res, line)                                                                            
+        idx = idx + 1                                                                                      
+    end                                                                                                    
+elseif rand == "1" then
+    for line in file:lines() do                                                                            
+        if idx == 100 then                                                                                   
+            break                                                                                          
+        end                                                                                                
+        if math.random(1, 1000) then
+            table.insert(res, line)                                                                            
+            idx = idx + 1                                                                                      
+        end
+    end                                                                                                    
+end
 
 ngx.print(json.encode(res))

cf304e38b1b60be3fc62cf8440780b1583500a7c
董家宏 <dongjiahong@hotmail.com>
2017-07-03 15:00:03 +0800

    添加点击图片事件

--- getImgUrls.lua
+++ getImgUrls.lua
@@ -1,14 +1,14 @@
 require "os"                                                                                           
 local json = require "cjson"                                                                           
 
---ngx.header['Access-Control-Allow-Origin'] = "*"                                                        
+ngx.header['Access-Control-Allow-Origin'] = "*"                                                        
 ngx.header['Content-Type'] = "application/json"                                                        
 math.randomseed(tostring(os.time()):reverse():sub(1, 7))
 
 local args = ngx.req.get_uri_args()
 local rand = args.rand or "0"
 
-os.execute(" ls -lh /pdata1/tmp_picture | sort -nk5 | awk '{if(NF>=9){print $9}}' > res")                                                                                                                             
+os.execute(" ls -lh /Users/lele/myGit/preview_img/images | sort -nk5 | awk '{if(NF>=9){print $9}}' > res")
 
 local res={ }                                                                                          
 local file = io.open("res")                                                                            
